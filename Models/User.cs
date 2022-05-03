295cda000e8d013bcdf08a187c4c2226bc33ba94
Suragowni <suresh-gowd.suragowni@capgemini.com>
2022-05-03 10:25:17 +0530

    commited

---
+++ Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Day6MvcApp.Models;
+using System.Web.Mvc;
+
+namespace Day6MvcApp.Models
+{
+    public class User
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+    }
+}
